3703d9566fcafc0bce81cc30e2061e51f528f639
Willis Allstead <willallstead@icloud.com>
2016-09-15 16:30:26 -0700

    Added pearson code as per my professor

---
+++ project1/PA01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "LinkedList.h"
+#include "Node.h"
+#include "PrecondViolatedExcept.h"
+
+using namespace std;
+
+int main() {
+    cout << "test" << endl;
+    return 0;
+}
